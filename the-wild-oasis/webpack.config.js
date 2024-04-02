65bc990228b4f91a80acde8d9b5c50f2feb728ca
ksachin7 <isachin.imp@gmail.com>
2024-04-02 13:57:42 +0530

    fix: updated webpack config

--- the-wild-oasis/webpack.config.js
+++ the-wild-oasis/webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-// const ESLintPlugin = require('eslint-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -13,100 +12,101 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "bundle.[name].[contenthash].js",
-        // publicPath: "/", // required for font loading on historyApiFallback
     },
     devServer: {
-        // contentBase: './dist',
         static: {
             directory: path.join(__dirname, 'public'),
         },
         port: 3000,
         hot: true,
-        historyApiFallback: true, // This option is used to allow react-router-dom to handle routing
+        open: true,
+        historyApiFallback: true,
     },
     module: {
         rules: [
             {
                 test: /\.(js|jsx|ts|tsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader'],
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
             },
             {
                 test: /\.css$/,
                 use: [
                     process.env.NODE_ENV === "production"
                         ? MiniCssExtractPlugin.loader
-                        : "style-loader",
-                    "css-loader",
+                        : "style-loader", "css-loader",
                 ],
             },
             {
                 test: /\.scss$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-                // use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.(png|jpe?g|gif|svg)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',   // Keep the original file name
-                            // outputPath: 'assets/',  // Place assets in the 'assets' folder inside 'dist'
-                        },
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {},
-                    },
-                ]
+                type: 'asset/resource',
+                generator: {
+                  filename: 'assets/[name][ext]',
+                },
             }
-            // {
-            //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            //     type: 'asset/resource',
-            // },
         ],
     },
     resolve: {
-        // modules: ['node_modules', 'src'],
-        extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],   // specifies which file extensions Webpack should resolve.
+        extensions: ["*", ".js", ".jsx", ".ts", ".tsx", ".json"],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components')
+        }
     },
     plugins: [
-        // new BundleAnalyzerPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         new MiniCssExtractPlugin({ filename: "styles.css" }),
         new HtmlWebpackPlugin({
             template: "./public/index.html",
-            filename: 'index.html', // Output filename
-            debug: true, // Enable debug mode
-            // excludeChunks: ['node_modules'], // Exclude node_modules chunk
+            filename: 'index.html',
+            debug: true,
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ['**/*', '!assets/*', '!.old-bundles/**'],
         }),
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'public', to: '', globOptions: { ignore: ['**/index.html'] } }, // Exclude index.html
+                {
+                    from: 'public',
+                    to: 'assets',  
+                    globOptions: {
+                        ignore: ['**/index.html'],  
+                    },
+                },
+                {
+                    from: 'dist',
+                    to: '.old-bundles',
+                    globOptions: {
+                        ignore: [
+                            'index.html',
+                            '**/assets/**',          
+                            '**/.old-bundles/**',   
+                            'bundle.*.js',          
+                        ],
+                    },
+                },
             ],
         }),
-        
-        // new ESLintPlugin({
-        //     extensions: ['js', 'jsx', 'ts', 'tsx'],
-        // Other options...
-        // }),
     ],
-    // externals: [
-    //     {
-    //       'cross-fetch': 'fetch'
-    //     }
-    //   ]
     optimization: {
         minimize: true,
         minimizer: [
-            new TerserPlugin({
-                // terser options
-            })
-        ]
+            new TerserPlugin({}),
+        ],
+        splitChunks: {
+            chunks: 'all'
+        }
     },
     performance: {
-        maxAssetSize: 244 * 1024,       // 244 KiB
-        maxEntrypointSize: 244 * 1024,  // 244 KiB
+        maxAssetSize: 244 * 1024,
+        maxEntrypointSize: 244 * 1024,
     },
 };
